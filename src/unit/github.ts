7450b18400537c741167137688d4082fef78cfdb
John Doe <xieqiqiang00@live.com>
2021-11-15 10:50:39 +0800

    C

--- src/unit/github.ts
+++ src/unit/github.ts
@@ -4,21 +4,14 @@ import { Notification } from '../unit/type';
 
 export async function GetGithubNotifications(): Promise<Notification[]> {
     const API_KEY: string = GithubAPIKEY()
-    let xhr = new XMLHttpRequest()
-    xhr.open("GET", `https://api.github.com/notifications?page=1&per_page=100`, true)
-    xhr.send()
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            try {
-                let data = JSON.parse(xhr.responseText);
-                let response: string[] = []
-                for (let url of data) {
-                    response.push(String(url))
-                }
-            } catch (error) {
-            }
+    const response = await fetch(`https://api.github.com/notifications?page=1&per_page=100`, {
+        method: 'GET', headers: {
+            "Content-Type": "application/json",
+            Authorization: `token ${API_KEY}`
         }
-    }
-return []
+    })
+    const data = await response.json();
+console.log("data",data)
+    return []
 }
 
